60a364165d89e78925b7a17dd59b3d2398ee8423
Peter Rowlands <peter@pmrowla.com>
2017-01-19 18:24:34 +0900

    working basic functionality

--- sopel_modules/sopel_discord/sopel_discord.py
+++ sopel_modules/sopel_discord/sopel_discord.py
@@ -1,18 +1,185 @@
 # coding=utf-8
 
-from __future__ import unicode_literals, absolute_import, division, print_function
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    division,
+    print_function
+)
+
+import threading
+import re
 
 from sopel import module
+from sopel.config.types import (
+    StaticSection, ValidatedAttribute, BaseValidated, NO_DEFAULT
+)
+from sopel.tools import get_input
+
+import discord
+
+import requests
+from requests.exceptions import HTTPError
+
+
+discord_api_url = 'https://discordapp.com/api'
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged into Discord as')
+    print(client.user.name)
+    print(client.user.id)
+    print('----')
+
+
+@client.event
+async def on_message(message):
+    if message.channel.id in client.channel_mappings \
+            and not message.author.bot \
+            and re.match(r'^(?![.!?].*$', message.content):
+        irc_channel = client.channel_mappings[message.channel.id]
+        irc_message = '<{}> {}'.format(message.author.name, message.content)
+        client.irc_bot.msg(irc_channel, irc_message)
+
+
+class DictAttribute(BaseValidated):
+    '''Config attribute containing a list of key: value pairs.
+
+    Key: value pairs are saved to the file as a comma-separated list.
+    The spaces before and after each item are stripped.
+    '''
+    def __init__(self, name, default=None):
+        default = default or {}
+        super(DictAttribute, self).__init__(name, default=default)
+
+    def parse(self, value):
+        pairs = value.split(',')
+        value = {}
+        for item in pairs:
+            k, v = item.split(':')
+            value[k.strip()] = v.strip()
+        return value
+
+    def serialize(self, value):
+        if not isinstance(value, dict):
+            raise ValueError('DictAttribute value must be a dict')
+        return ','.join(['{}:{}'.format(k, v) for k, v in value.items()])
+
+    def configure(self, prompt, default, parent, section_name):
+        each_prompt = '?'
+        if isinstance(prompt, tuple):
+            each_prompt = prompt[1]
+            prompt = prompt[0]
+
+            if default is not NO_DEFAULT:
+                prompt = '{} [{}]'.format(prompt, default)
+            else:
+                default = ''
+            values = []
+            value = get_input(each_prompt + ' ') or default
+            while value:
+                values.append(value)
+                value = get_input(each_prompt + ' ')
+            return self.parse(','.join(values))
+
+
+class DiscordSection(StaticSection):
+    discord_token = ValidatedAttribute('discord_token')
+    channel_mappings = DictAttribute('channel_mappings')
+
+
+def _setup_webhooks(bot):
+    bot.memory['webhooks'] = {}
+    headers = {
+        'Authorization': 'Bot {}'.format(bot.config.discord.discord_token)
+    }
+    for k, channel_id in bot.memory['channel_mappings'].items():
+        try:
+            r = requests.get(
+                '{}/channels/{}/webhooks'.format(discord_api_url, channel_id),
+                headers=headers
+            )
+            bot.memory['webhooks'][channel_id] = {}
+            r.raise_for_status()
+            for hook in r.json():
+                if hook['name'] == 'discord-irc':
+                    bot.memory['webhooks'][channel_id] = hook
+            if not bot.memory['webhooks'][channel_id]:
+                payload = {'name': 'discord-irc'}
+                r = requests.post(
+                    '{}/channels/{}/webhooks'.format(
+                        discord_api_url,
+                        channel_id
+                    ),
+                    headers=headers,
+                    json=payload
+                )
+                r.raise_for_status()
+                bot.memory['webhooks'][channel_id] = r.json()
+        except HTTPError as e:
+            print('Could not access webhook API for channel {}.'.format(
+                channel_id))
+            print('Make sure the bot user has the "Manage webhooks" permission'
+                  'on the specified discord channel.')
+            print(e)
 
 
 def configure(config):
-    pass
+    config.define_section('discord', DiscordSection)
+    config.discord.configure_setting(
+        'discord_token',
+        'Discord token for the app bot user'
+    )
+    config.discord.configure_setting(
+        'channel_mappings',
+        ('Comma-separated list of Discord channel to IRC channel mappings'
+         ' (ex: #discord-channel1: #irc-channel1,'
+         ' #discord-channel2: #irc-channel2)')
+    )
 
 
 def setup(bot):
-    pass
+    bot.config.define_section('discord', DiscordSection)
+    client.irc_bot = bot
+    client.channel_mappings = bot.config.discord.channel_mappings
+    print(client.channel_mappings)
+    # config order maps discord: IRC, invert the map for the IRC bot
+    bot.memory['channel_mappings'] = {
+        v: k for k, v in client.channel_mappings.items()
+    }
+    _setup_webhooks(bot)
+    targs = (bot.config.discord.discord_token,)
+    t = threading.Thread(target=client.run, args=targs)
+    t.start()
 
 
-@module.commands('helloworld')
-def hello_world(bot, trigger):
-    bot.say('Hello, world!')
+# Match all messages except for those which start with common bot command
+# prefixes
+@module.require_chanmsg
+@module.rule(r'^(?![.!?]).*$')
+def irc_message(bot, trigger):
+    if not trigger.is_privmsg \
+            and trigger.sender in bot.memory['channel_mappings']:
+        discord_channel = bot.memory['channel_mappings'][trigger.sender]
+        hook = bot.memory['webhooks'].get(discord_channel, {})
+        if hook:
+            headers = {
+                'Authorization': 'Bot {}'.format(
+                    bot.config.discord.discord_token)
+            }
+            payload = {
+                'content': trigger.match.string,
+                'username': '{} (IRC)'.format(trigger.nick),
+            }
+            try:
+                r = requests.post('{}/webhooks/{}/{}'.format(
+                        discord_api_url, hook['id'], hook['token']
+                    ),
+                    headers=headers,
+                    json=payload,
+                )
+                r.raise_for_status()
+            except HTTPError as e:
+                pass
